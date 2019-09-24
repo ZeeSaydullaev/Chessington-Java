095fab3a4e660d6415a1850e05cd7ce55908a4f1
Daniel Field <dannyf506@googlemail.com>
2019-09-24 15:06:18 +0100

    refactored code that passes tests 1 and 2

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -17,24 +17,27 @@ public class Pawn extends AbstractPiece {
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
 
         List<Move> moves = new ArrayList<>();
-        
-        if(from.getRow() == 1 && getColour() == PlayerColour.BLACK) {
-           Move initMove = new Move(from, from.plus(2,0));
-            moves.add(initMove);
-        } else if(from.getRow() == 6 && getColour() == PlayerColour.WHITE) {
-            Move initMove = new Move(from, from.plus(-2,0));
+
+        if (isStartingPawnRow(from)) {
+            Move initMove = getColour() == PlayerColour.BLACK ?
+                    new Move(from, from.plus(2, 0)) :
+                    new Move(from, from.plus(-2, 0));
             moves.add(initMove);
         }
-        
-        
-        
 
         Move moveForward = getColour() == PlayerColour.WHITE ?
-                new Move(from, from.plus(-1,0)) :
-                new Move(from, from.plus(1,0));
-
+                new Move(from, from.plus(-1, 0)) :
+                new Move(from, from.plus(1, 0));
         moves.add(moveForward);
 
         return moves;
+    }
+
+    private boolean isStartingPawnRow(Coordinates start) {
+        if (start.getRow() == 1 && getColour() == PlayerColour.BLACK || start.getRow() == 6 && getColour() == PlayerColour.WHITE) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
